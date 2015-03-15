65d033ac085f7b9ef594b103587270fbc18c33b1
halogenandtoast <halogenandtoast@gmail.com>
2015-03-15 10:28:07 -0400

    [WIP] Generating rooms

--- book/dungeon_generation/generating_rooms.md
+++ book/dungeon_generation/generating_rooms.md
@@ -1,19 +1,133 @@
 ## Chapter 8 - Generating random rooms
 
-Generating random rooms is one of the most complicated and interesting parts of writing a roguelike. You want each level of a dungeon to feel unique. At the same time, you want it to feel very random.
+Here `xlim` and `ylim` represent the amount of horizontal and vertical space we want around our room. The conditional specified in each check is to verify that the rectangle we're placing our new rectangle into does not take up the entire width or height of our screen. If it doesn't take up the full width or height we multiply `xlim` or `ylim` by 2 respectively. This value is the "padding" on for both sides. If we are placing it into a rectangle that takes up the full width or height, then we know if can't possibly be next to another rectangle in that dimension and therefor we just want to offset it a little from the corresponding side.
 
-Rooms in nethack are fairly simple. They are always rectangles. A room in NetHack is represented by horizontal walls `-`, vertical walls `|` and the floor `.`. Let's start by figuring out how we're going to randomly generate a rectangle. What we're going to do is start by treating the entire screen as an initial rectangle. In NetHack this rectangle is 80 columns by 21 rows. Then we'll try to find a random rectangle that fits inside of it.
-
-We're going to need to set some rules for the rooms though. We want our rectangles to have a random width and height, but we don't want them to be too big or too small. In order to make this work we'll need to use `rand` to get some reasonable values:
+After we've determined our width, height, xborder, and yborder we'll want to figure out where in the current rectangle we're going to put this new rectangle:
 
 ```ruby
-width = 2 + rand((rect.right - rect.left > 28) ? 12 : 8)
-height = 2 + rand(4)
+xabs = rect.left +
+  (rect.left > 0 ? xlim : 3) +
+  rand(rect.right - (rect.left > 0 ? rect.left : 3) - dx - xborder + 1)
+
+yabs = rect.top +
+  (rect.top > 0 ? ylim: 2) +
+  rand(rect.bottom - (rect.top > 0 ? rect.top : 2) - dy - yborder + 1)
 ```
 
-Here you can see that our rectangle has a minimum width and height of 2. The maximum width is either 14 or 10 depending on how big the containing rectangle is.
+There's quite a bit to decipher here so let's break it down. To find an x position we'll first start from the left side of our containing rectangle, we then inset our rectangle just a little (we don't want two rectangles to end up directly next to each other). Next we add a random amount, but we want to make sure we have enough room so we don't want to generate a number that would cause our rectangle to be outside of our containing rectangle. To ensure we don't run into this situation we take the right side and subtract either the left (if not zero) or 3 (for an inset). Then we substract the width and xborder and then add 1 since `0 <= rand(x) < x`.
 
-The usage or rectangles simplifies a lot of equations for us. For one, it's very easy to tell if two rectangles overlap using axis-aligned bounding boxes or AABBs. AABBs see frequent usage in shoot-em up games, due to the simplicity of the equation involved. Basically, in order to check if two triangles overlap you have to make the following 4 checks:
+Once we've determined the `xabs`,`yabs`, `dx` and `dy` we have a rectangle. Let's draw our rectanle on the screen. We can do this in 3 steps, first we'll draw the horizontal walls. Here we'll draw the entire top row and bottom row of our room with `-`:
+
+```ruby
+[top - 1, bottom + 1].each do |y|
+  (left - 1).upto(right + 1) do |x|
+    dungeon[y][x] = HWALL
+  end
+end
+```
+
+For the vertical walls we'll do something similar, but we'll use `|`s for the left and right walls:
+
+```ruby
+top.upto(bottom) do |y|
+  [left - 1, right + 1].each do |x|
+    dungeon[y][x] = VWALL
+  end
+end
+```
+
+Finally we'll draw in the floor with `.`s:
+
+```ruby
+top.upto(bottom) do |y|
+  left.upto(right) do |x|
+    dungeon[y][x] = FLOOR
+  end
+end
+```
+
+Now we can start printing our random rooms to the screen. Here is a full listing of the code used for generating and drawing a single random room:
+
+```ruby
+class Rect < Struct.new(:left, :top, :right, :bottom); end
+
+class Dungeon
+  COLNO = 80
+  ROWNO = 21
+
+  STONE = " "
+  HWALL = "-"
+  VWALL = "|"
+  FLOOR = "."
+
+  def initialize
+    @dungeon = Array.new(ROWNO) { Array.new(COLNO) { STONE } }
+    @rects = [ Rect.new(0, 0, COLNO - 1, ROWNO - 1) ]
+  end
+
+  def generate
+    create_room
+    print_dungeon
+  end
+
+  private
+
+  attr_reader :dungeon, :rects
+
+  def print_dungeon
+    dungeon.each do |row|
+      puts row.join
+    end
+  end
+
+  def create_room
+    xlim = 4
+    ylim = 3
+
+    rect = rects.sample
+
+    dx = 2 + rand((rect.right - rect.left > 8) ? 12 : 8)
+    dy = 2 + rand(4)
+
+    xborder = (rect.left > 0 && rect.right < COLNO - 1) ? 2 * xlim : xlim + 1
+    yborder = (rect.top > 0 && rect.bottom < ROWNO - 1) ? 2 * ylim : ylim + 1
+
+    xabs = rect.left +
+      (rect.left > 0 ? xlim : 3) +
+      rand(rect.right - (rect.left > 0 ? rect.left : 3) - dx - xborder + 1)
+
+    yabs = rect.top + 
+      (rect.top > 0 ? ylim : 2) +
+      rand(rect.top - (rect.bottom > 0 ? rect.top : 2) - dy - yborder + 1)
+
+    add_room(xabs, yabs, xabs + dx, yabs + dy)
+  end
+
+  def add_room(left, top, right, bottom)
+    [top - 1, bottom + 1].each do |y|
+      (left - 1).upto(right + 1) do |x|
+        dungeon[y][x] = HWALL
+      end
+    end
+
+    top.upto(bottom) do |y|
+      [left - 1, right + 1].each do |x|
+        dungeon[y][x] = VWALL
+      end
+
+      left.upto(right) do |x|
+        dungeon[y][x] = FLOOR
+      end
+    end
+  end
+end
+
+Dungeon.new.generate
+```
+
+Now that we've generated a single room we're going to want to generate more of course. The gist of how we'll do this is by splitting our dungeon into new rectangles when we've placed a room. First we'll start with a list with a single rectangle (the entire dungeon) in it. We randomly select a rectangle and then generate a random room to fit into it. We then split that rectangle into 4 more rectangles that will surround our room. We'll discard any rectangles that are too small to fit a room into and then put the sub rectangles back into the list. We're done adding rooms when either the list is empty or we've added a maximum number of rooms. This process is easier to understand once visualized:
+
+It's very easy to tell if two rectangles overlap using axis-aligned bounding boxes or AABBs. AABBs see frequent usage in shoot-em up games, due to the simplicity of the equation involved. Basically, in order to check if two triangles overlap you have to make the following 4 checks:
 
 1. The top of triangle A is higher than the bottom of triangle B
 2. The bottom of triangle A is lower than the top of triangle B
--- example_app/dungeon.rb
+++ example_app/dungeon.rb
@@ -1,9 +1,35 @@
+require 'pp'
 require 'pry'
 
-class Rect < Struct.new(:lx, :ly, :hx, :hy); end
+class Coord < Struct.new(:x, :y)
+  def self.init
+    new.tap do |coord|
+      coord.x = 0
+      coord.y = 0
+    end
+  end
+end
+
+class Rect < Struct.new(:lx, :ly, :hx, :hy)
+  def self.init
+    new.tap do |rect|
+      rect.lx = 0
+      rect.ly = 0
+      rect.hx = 0
+      rect.hy = 0
+    end
+  end
+end
+
+class Room < Rect; end
 
 class Dungeon
-  MAX_ROOM_COUNT = 40
+  STONE = " "
+  HWALL = "-"
+  VWALL = "|"
+  ROOM = "."
+  MAXNROFROOMS = 40
+  MAXRECT = 40
 
   COLNO = 80
   ROWNO = 21
@@ -11,48 +37,333 @@ class Dungeon
   XLIM = 4
   YLIM = 3
 
+  attr_reader :rect, :rooms, :locations
+
   def initialize
-    @rects = [ Rect.new(0, 0, COLNO - 1, ROWNO - 1) ]
+    @rooms = []
+    @rect = [ Rect.new(0, 0, COLNO-1, ROWNO-1) ]
+
+    @locations = Array.new(ROWNO) { Array.new(COLNO) { STONE} }
+  end
+
+  def print_rects
+    grid = Array.new(ROWNO) { Array.new(COLNO) { " " } }
+    @rect.each_with_index do |rect, index|
+      char = (97 + index).chr
+      rect.ly.upto(rect.hy) do |y|
+        rect.lx.upto(rect.hx) do |x|
+          grid[y][x] = char
+        end
+      end
+      puts grid.map(&:join)
+      puts
+      puts
+    end
+
+    @locations.each_with_index do |row, y|
+      row.each_with_index do |col, x|
+        if col != STONE
+          grid[y][x] = col
+        end
+      end
+    end
+
+    puts grid.map(&:join)
+  end
+
+  def print_rooms
+    puts @locations.map(&:join)
   end
 
   def generate
-    make_rooms
+    makerooms
+    print_rooms
   end
 
   private
 
-  attr_reader :rects
-
-  def make_rooms
-    create_room
+  def makerooms
+    while (rooms.count < MAXNROFROOMS) && rect.any?
+      puts "*" * 80
+      puts
+      print_rects
+      gets
+      return unless create_room
+    end
   end
 
   def create_room
+    xabs = nil
+    yabs = nil
+    wtmp = nil
+    htmp = nil
+
+    r1 = rect.sample
+    r2 = Rect.init
+
+    trycnt = 0
+
     xlim = XLIM
     ylim = YLIM
 
-    r1 = random_rect
+    loop do
+      xaltmp = nil
+      yaltmp = nil
 
-    hx = r1.hx
-    hy = r1.hy
-    lx = r1.lx
-    ly = r1.ly
+      r1 = rect.sample
+      if !r1
+        return false
+      end
 
-    dx = 2 + rand((hx - lx > 8) ? 12 : 8)
-    dy = 2 + rand(4)
+      hx = r1.hx
+      hy = r1.hy
+      lx = r1.lx
+      ly = r1.ly
 
-    xborder = 5
-    yborder = 4
+      dx = 2 + rand((hx-lx > 28) ? 12 : 8)
+      dy = 2 + rand(4)
+      if dx*dy > 50
+        dy = 50/dx
+      end
 
-    xabs = lx + (lx > 0 ? xlim : 3) + rand(hx - (lx > 0 ? lx : 3) - dx - xborder + 1)
-    yabs = ly + (ly > 0 ? ylim : 2) + rand(hy - (ly > 0 ? ly : 2) - dy - yborder + 1)
+      xborder = (lx > 0 && hx < COLNO - 1) ? 2*xlim : xlim+1
+      yborder = (ly > 0 && hy < ROWNO - 1) ? 2*ylim : ylim+1
 
-    Rect.new(xabs - 1, yabs - 1, xabs + dx + 1, yabs + dy + 1)
+      if (hx-lx < (dx + 3 + xborder)) || (hy-ly < (dy + 3 + yborder))
+        r1 = nil
+        trycnt += 1
+        break if trycnt > 100
+        next
+      end
+
+      xabs = lx + (lx > 0 ? xlim : 3) + rand(hx - (lx > 0 ? lx : 3) - dx - xborder + 1)
+      yabs = ly + (ly > 0 ? ylim : 2) + rand(hy - (ly > 0 ? ly : 2) - dy - yborder + 1)
+
+      if ly == 0 && hy >= (ROWNO - 1) && (rooms.count == 0 || rand(rooms.count) == 0) && (yabs + dy > ROWNO/2)
+        yabs = rand(3) + 2
+        if rooms.count < 4 && dy > 1
+          dy -= 1
+        end
+      end
+
+      success, xabs, dx, yabs, dy = check_room(xabs, dx, yabs, dy)
+      unless success
+        r1 = nil
+        trycnt += 1
+        break if trycnt > 100
+        next
+      end
+
+      wtmp = dx+1
+      htmp = dy+1
+
+      r2.lx = xabs - 1
+      r2.ly = yabs - 1
+      r2.hx = xabs + wtmp
+      r2.hy = yabs + htmp
+
+      trycnt += 1
+      break if trycnt > 100 || r1
+    end
+
+    if !r1
+      return false
+    end
+
+    r = split_rects(r1, r2)
+
+    add_room(xabs, yabs, xabs+wtmp-1, yabs+htmp-1)
+
+    true
   end
 
-  def random_rect
-    rects.sample
+  def add_room(lowx, lowy, hix, hiy)
+    croom = Room.init
+    do_room_or_subroom(croom, lowx, lowy, hix, hiy, true)
+
+    rooms << croom
+  end
+
+  def do_room_or_subroom(croom, lowx, lowy, hix, hiy, is_room)
+    if lowx == 0
+      lowx = 1
+    end
+
+    if lowy == 0
+      lowy = 1
+    end
+
+    if hix >= COLNO - 1
+      hix = COLNO - 2
+    end
+
+    if hiy >= ROWNO - 1
+      hiy = ROWNO - 2
+    end
+
+    croom.lx = lowx
+    croom.hx = hix
+    croom.ly = lowy
+    croom.hy = hiy
+
+    (lowy-1).step(hiy+1, hiy - lowy + 2) do |y|
+      (lowx-1).upto(hix+1) do |x|
+        locations[y][x] = HWALL
+      end
+    end
+
+    lowy.upto(hiy) do |y|
+      (lowx-1).step(hix+1, hix-lowx+2) do |x|
+        locations[y][x] = VWALL
+      end
+    end
+
+    lowy.upto(hiy) do |y|
+      lowx.upto(hix) do |x|
+        locations[y][x] = ROOM
+      end
+    end
+  end
+
+  def split_rects(r1, r2)
+    old_r = r1.dup
+    remove_rect(r1)
+
+    (rect.count-1).downto(0) do |i|
+      success, r = intersect?(rect[i], r2)
+      if success
+        split_rects(rect[i], r)
+      end
+    end
+
+    if r2.ly - old_r.ly - 1 > (old_r.hy < ROWNO - 1 ? 2 * YLIM : YLIM + 1) + 4
+      r = old_r.dup
+      r.hy = r2.ly - 2
+      add_rect(r)
+    end
+
+    if r2.lx - old_r.lx - 1 > (old_r.hx < COLNO - 1 ? 2 * XLIM : XLIM + 1) + 4
+      r = old_r.dup
+      r.hx = r2.lx - 2
+      add_rect(r)
+    end
+
+    if old_r.hy - r2.hy - 1 > (old_r.ly > 0 ? 2 * YLIM : YLIM + 1) + 4
+      r = old_r.dup
+      r.ly = r2.hy + 2
+      add_rect(r)
+    end
+
+    if old_r.hx - r2.hx - 1 > (old_r.lx > 0 ? 2 * XLIM : XLIM + 1) + 4
+      r = old_r.dup
+      r.lx = r2.hx + 2
+      add_rect(r)
+    end
+  end
+
+  def intersect?(r1, r2)
+    if r2.lx > r1.hx || r2.ly > r1.hy || r2.hx < r1.lx || r2.hy < r1.ly
+      return false
+    end
+
+    r3 = Rect.init
+    r3.lx = (r2.lx > r1.lx ? r2.lx : r1.lx)
+    r3.ly = (r2.ly > r1.ly ? r2.ly : r1.ly)
+    r3.hx = (r2.hx > r1.hx ? r1.hx : r2.hx)
+    r3.hy = (r2.hy > r1.hy ? r1.hy : r2.hy)
+
+    if r3.lx > r3.hx || r3.ly > r3.hy
+      return false
+    end
+
+    [true, r3]
+  end
+
+  def add_rect(r)
+    if rect.count >= MAXRECT
+      return
+    end
+
+    if get_rect(r)
+      return
+    end
+
+    rect << r
+  end
+
+  def remove_rect(r)
+    rect.delete(r)
+  end
+
+  def get_rect(r)
+    lx = r.lx
+    ly = r.ly
+    hx = r.hx
+    hy = r.hy
+    0.upto(rect.count - 1) do |i|
+      rectp = rect[i]
+      if lx >= rectp.lx && ly >= rectp.ly && hx <= rectp.hx && hy <= rectp.hy
+        return rectp
+      end
+    end
+
+    nil
+  end
+
+  def check_room(lowx, ddx, lowy, ddy)
+    hix = lowx + ddx
+    hiy = lowy + ddy
+
+    xlim = XLIM
+    ylim = YLIM
+
+    if lowx < 3
+      lowx = 3
+    end
+
+    if lowy < 2
+      lowy = 2
+    end
+
+    if hix > COLNO - 3
+      hix = COLNO - 3
+    end
+
+    if hiy > ROWNO - 3
+      hiy = ROWNO - 3
+    end
+
+    # chk
+
+    if hix <= lowx || hiy <= lowy
+      return false
+    end
+
+    (lowx - xlim).upto(hix + xlim) do |x|
+      if x<= 0 || x >= COLNO
+        next
+      end
+
+      y = lowy - ylim
+      ymax = hiy + ylim
+      if y < 0
+        y = 0
+      end
+
+      if ymax >= ROWNO
+        ymax = ROWNO - 1
+      end
+
+    end
+
+    ddx = hix - lowx
+    ddy = hiy - lowy
+
+    [true, lowx, ddx, lowy, ddy]
   end
 end
 
-puts Dungeon.new.generate
+seed = ENV["SEED"] || Random::DEFAULT.seed
+srand(seed.to_i)
+puts "SEED=#{seed}"
+Dungeon.new.generate
